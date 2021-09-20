242bb5a30ac1e001029d01f0ceb6bf5731c4ea59
Eliyahu Gamliel <egamliel@g.jct.ac.il>
2021-09-20 11:00:42 +0300

    Initial code + Welcome functions

---
+++ Targil0/Program4054.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4054();
+            Welcome4567();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome4567();
+
+        private static void Welcome4054()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+    }
+}
