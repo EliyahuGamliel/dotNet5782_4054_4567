0856a3d2c810aa10a2d00ae8cef1c45ad599a452
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-15 09:17:43 +0300

    Update Class1.cs

--- DAL/Class1.cs
+++ DAL/Class1.cs
@@ -26,11 +26,11 @@ namespace IDAL
             public int Targetld { get; set; }
             public WeightCategories Weight { get; set; }
             public Priorities Lattitube{ get; set; }
-            public datetime Requested{ get; set; }
+            public DateTime Requested{ get; set; }
             public int Droneld{ get; set; }
-            public Datetime Scheduled{ get; set; }
-            public Datetime PickedUp{ get; set; }
-            public Datetime Delivered{ get; set; }
+            public DateTime Scheduled{ get; set; }
+            public DateTime PickedUp{ get; set; }
+            public DateTime Delivered{ get; set; }
         }
         
        public struct Drone
