0565c312e6e629475f6427c668db7e84abe0a948
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-17 21:27:36 +0300

    Update Station.cs

--- DAL/Station.cs
+++ DAL/Station.cs
@@ -5,12 +5,15 @@ namespace IDAL
     namespace DO
     {
        public struct Station
-        {
-            public int Id { get; set; }
-            public int Name { get; set; }
-            public double Longitude { get; set; }
-            public double Lattitude{ get; set; }
-            public int ChargeSlots{ get; set; }
+       {
+           public int Id { get; set; }
+           public int Name { get; set; }
+           public double Longitude { get; set; }
+           public double Lattitude{ get; set; } 
+           public int ChargeSlots{ get; set; }
+           
+           public string override ToString() 
+           { return $"Id: {Id}\nName: {Name}\nLongitude: {Longitude}\nLattitude: {Lattitude}\nChargeSlots: {ChargeSlots}\n"; }      
         }  
     }
 }
