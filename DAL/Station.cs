d0a640ec9e891f28ae38f04a435633664b8cf695
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-18 14:19:53 +0300

    Update Station.cs

--- DAL/Station.cs
+++ DAL/Station.cs
@@ -13,7 +13,37 @@ namespace IDAL
            public int ChargeSlots{ get; set; }
            
            public string override ToString() 
-           { return $"Id: {Id}\nName: {Name}\nLongitude: {Longitude}\nLattitude: {Lattitude}\nChargeSlots: {ChargeSlots}\n"; }      
+           { return $"Id: {Id}\nName: {Name}\nLongitude: {Longitube_Bonus(Longitude)}\nLattitude: {Lattitube_Bonus(Lattitude)}\nChargeSlots: {ChargeSlots}\n"; }     
+           
+           public static string Longitube_Bonus(double num)
+            {
+            char dir = 'S';
+                if (num < 0)
+                    num = -num;
+                else
+                    dir = 'N';
+                int degrees = (int)num;
+                num = num - degrees;
+                int minutes = (int)(num * 60);
+                double rest = num - ((double)minutes / 60);
+                double seconds = rest * 3600;
+                return $"{degrees}° {minutes}' {Math.Round(seconds, 3, MidpointRounding.ToZero)}\" {dir}\n";
+            }
+            
+            public static string Lattitube_Bonus(double num)
+            {
+                char dir = 'W';
+                if (num < 0)
+                    num = -num;
+                else
+                    dir = 'E';
+                int degrees = (int)num;
+                num = num - degrees;
+                int minutes = (int)(num * 60);
+                double rest = num - ((double)minutes / 60);
+                double seconds = rest * 3600;
+                return $"{degrees}° {minutes}' {Math.Round(seconds, 3, MidpointRounding.ToZero)}\" {dir}\n";
+            }
         }  
     }
 }
