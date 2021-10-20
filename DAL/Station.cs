e535b890be2a0723f4c0f300a9165e63fbac95e9
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-20 18:08:57 +0300

    Update Station.cs

--- DAL/Station.cs
+++ DAL/Station.cs
@@ -16,7 +16,7 @@ namespace IDAL
            { return $"Id: {Id}\nName: {Name}\nLongitude: {Longitube_Bonus(Longitude)}\nLattitude: {Lattitube_Bonus(Lattitude)}\nChargeSlots: {ChargeSlots}\n"; }     
            
            public static string Longitube_Bonus(double num)
-            {
+           {
             char dir = 'S';
                 if (num < 0)
                     num = -num;
@@ -43,6 +43,19 @@ namespace IDAL
                 double rest = num - ((double)minutes / 60);
                 double seconds = rest * 3600;
                 return $"{degrees}° {minutes}' {Math.Round(seconds, 3, MidpointRounding.ToZero)}\" {dir}\n";
+            }
+           
+           public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+        {
+            double rlat1 = Math.PI * lat1 / 180;
+            double rlat2 = Math.PI * lat2 / 180;
+            double theta = lon1 - lon2;
+            double rtheta = Math.PI * theta / 180;
+            double dist = Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) * Math.Cos(rlat2) * Math.Cos(rtheta);
+            dist = Math.Acos(dist);
+            dist = dist * 180 / Math.PI;
+            dist = dist * 60 * 1.1515;
+            return Math.Round(dist * 1.609344, 2);
             }
         }  
     }
