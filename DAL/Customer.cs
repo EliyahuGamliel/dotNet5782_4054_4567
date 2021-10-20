c2325fed89c035410275eb4cd947b8f02f9224bc
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-20 18:09:45 +0300

    Update Customer.cs

--- DAL/Customer.cs
+++ DAL/Customer.cs
@@ -44,6 +44,19 @@ namespace IDAL
                 double seconds = rest * 3600;
                 return $"{degrees}° {minutes}' {Math.Round(seconds, 3, MidpointRounding.ToZero)}\" {dir}\n";
             }
+            
+            public static double DistanceTo(double lat1, double lon1, double lat2, double lon2, char unit = 'K')
+            {
+                double rlat1 = Math.PI * lat1 / 180;
+                double rlat2 = Math.PI * lat2 / 180;
+                double theta = lon1 - lon2;
+                double rtheta = Math.PI * theta / 180;
+                double dist = Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) * Math.Cos(rlat2) * Math.Cos(rtheta);
+                dist = Math.Acos(dist);
+                dist = dist * 180 / Math.PI;
+                dist = dist * 60 * 1.1515;
+                return Math.Round(dist * 1.609344, 2);
+            }
         }
     }
 }
