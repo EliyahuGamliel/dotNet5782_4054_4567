d09f51a493ffc5c46c0acbb9035976fff98bc29f
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-24 19:48:40 +0300

    Update Parcel.cs

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -7,12 +7,12 @@ namespace IDAL
         public struct Parcel
         {
             public int Id { get; set; }
-            public int Senderld { get; set; }
-            public int Targetld { get; set; }
+            public int SenderId { get; set; }
+            public int TargetId { get; set; }
             public WeightCategories Weight { get; set; }
             public Priorities priority{ get; set; }
             public DateTime Requested{ get; set; }
-            public int Droneld{ get; set; }
+            public int DroneId{ get; set; }
             public DateTime Scheduled{ get; set; }
             public DateTime PickedUp{ get; set; }
             public DateTime Delivered{ get; set; }
