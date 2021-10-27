00cc04b463400c99e904f09006b3c930cb073ecb
EliyahuGamliel <76405628+EliyahuGamliel@users.noreply.github.com>
2021-10-27 17:41:40 +0300

    targil2.E1

---
+++ BL/enums.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace IBL
+{
+    namespace BO
+    {
+        /// <summary>
+        /// Set up all the enums relevant to the program
+        /// </summary>
+        public enum WeightCategories {Light, Medium, Heavy}
+        
+        public enum DroneStatuses {Available, Maintenance, Delivery}
+        
+        public enum Priorities {Normal, Fast, Emergency}
+    }
+}
