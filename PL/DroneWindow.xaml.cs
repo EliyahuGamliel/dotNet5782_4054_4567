8783aef6dc6dc787d8be31a8d00b0b6c04182db8
Eliyahu Gamliel <egamliel@g.jct.ac.il>
2021-11-28 22:48:22 +0200

    targil3.I

--- PL/DroneWindow.xaml.cs
+++ PL/DroneWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using IBL.BO;
 
 namespace PL
 {
@@ -19,9 +20,10 @@ namespace PL
     /// </summary>
     public partial class DroneWindow : Window
     {
-        public DroneWindow()
+        public DroneWindow(IBL.IBL bl)
         {
             InitializeComponent();
+
         }
     }
 }
