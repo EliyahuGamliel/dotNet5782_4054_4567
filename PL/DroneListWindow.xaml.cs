59bcfe3e7a05d4619acef9ed38adf43cb11c0a6c
Eliyahu Gamliel <egamliel@g.jct.ac.il>
2021-11-28 22:47:50 +0200

    targil3.I

--- PL/DroneListWindow.xaml.cs
+++ PL/DroneListWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using IBL.BO;
 
 namespace PL
 {
@@ -24,8 +25,35 @@ namespace PL
         {
             InitializeComponent();
             bl_drones = bl;
-            DroneListView.ItemsSource = bl.GetDrones();
-          
+            StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatuses));
+            MaxWeightSelector.ItemsSource = Enum.GetValues(typeof(WeightCategories));
+            DroneListView.ItemsSource = bl_drones.GetDroneByFilter(d => true);
+                      
+        }
+
+        private void StatusSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DroneListView.ItemsSource = bl_drones.GetDroneByFilter(d => (int)d.Status == (int)StatusSelector.SelectedItem);
+        }
+
+        private void MaxWeightSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DroneListView.ItemsSource = bl_drones.GetDroneByFilter(d => (int)d.MaxWeight == (int)MaxWeightSelector.SelectedItem);
+        }
+
+        private void AddDrone_Click(object sender, RoutedEventArgs e)
+        {
+            new DroneWindow(bl_drones).Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void action_drone(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
