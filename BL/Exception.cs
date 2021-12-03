db862c628cbc885279d9e341c5690ca0bc2b8b96
israel <isracohe@g.jct.ac.il>
2021-12-03 10:58:18 +0200

    israel

--- BL/Exception.cs
+++ BL/Exception.cs
@@ -87,10 +87,20 @@ namespace IBL.BO
     public class ChoiceNotLegal : Exception
     {
         public double choice { get; private set; }
+
+        /// <summary>
+        /// if the choice is not legal then it assigns the choice
+        /// </summary>
+        /// <param name="choice">the choice</param>
         public ChoiceNotLegal(int choice)
         {
             this.choice = choice;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "ChoiceNotLegal: The choice it is not in the options (" + choice + ")\n";
@@ -104,10 +114,20 @@ namespace IBL.BO
     public class PhoneExistException : Exception
     {
         public string phone { get; private set; }
+
+        // <summary>
+        /// if the phone number already exists then it assigns the phone number
+        /// </summary>
+        /// <param name="phone">the phone number</param>
         public PhoneExistException(string Phone)
         {
             this.phone = Phone;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "PhoneExistException: The Phone " + phone + " exist\n";
@@ -121,10 +141,20 @@ namespace IBL.BO
     public class SameCustomerException : Exception
     {
         public int id { get; private set; }
+
+        /// <summary>
+        /// if the the customer already exists the it assigns the id
+        /// </summary>
+        /// <param name="id">the id</param>
         public SameCustomerException(int id)
         {
             this.id = id;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "SameCustomersException: The TargetId is like the SenderID (" + id + ")\n";
@@ -137,6 +167,11 @@ namespace IBL.BO
     [Serializable]
     public class DroneCannotSend : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "DroneCanNotSend: The Drone cann't send to charge\n";
@@ -149,6 +184,11 @@ namespace IBL.BO
     [Serializable]
     public class DroneCannotRelese : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "DroneCanNotRelese: The Drone isn't in charge\n";
@@ -162,10 +202,20 @@ namespace IBL.BO
     public class TimeNotLegal : Exception
     {
         public double time { get; private set; }
+
+        /// <summary>
+        /// if the time is not legal then it assigns the time
+        /// </summary>
+        /// <param name="time">the time</param>
         public TimeNotLegal(double time)
         {
             this.time = time;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "TimeNotLegal: The Drone cann't relese frome charging, because the time is not legal (" + time + ")\n";
@@ -178,6 +228,11 @@ namespace IBL.BO
     [Serializable]
     public class DroneCannotAssigan : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "DroneCanNotAssigan: The Drone cann't assign to parcel\n";
@@ -190,6 +245,11 @@ namespace IBL.BO
     [Serializable]
     public class DroneCannotPickUp : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "DroneCanNotPickUp: The Drone cann't pick up the parcel\n";
@@ -202,6 +262,11 @@ namespace IBL.BO
     [Serializable]
     public class DroneCannotDeliver : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "DroneCanNotDeliver: The Drone cann't deliver the parcel\n";
@@ -214,6 +279,11 @@ namespace IBL.BO
     [Serializable]
     public class StationIsFull : Exception
     {
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "StationIsFull: The Drone cann't send/add because the station is full\n";
@@ -228,11 +298,22 @@ namespace IBL.BO
     {
         public double longitude { get; private set; }
         public double latitude { get; private set; }
+
+        /// <summary>
+        /// if the locatiom is not legal then it assigns the location
+        /// </summary>
+        /// <param name="longitude">the longitude</param>
+        /// <param name="latitude">the latitude</param>
         public LocationNotLegal(double longitude, double latitude)
         {
             this.longitude = longitude;
             this.latitude = latitude;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "LocationNotLegal: The Location doesn't exist (" + longitude +  ", " + latitude + ")\n";
@@ -246,10 +327,20 @@ namespace IBL.BO
     public class ChargeSlotsNotLegal : Exception
     {
         public int chargeSlots { get; private set; }
+
+        /// <summary>
+        /// if the chargeslot is not legal the it assigns the charge slots
+        /// </summary>
+        /// <param name="chargeSlots"></param>
         public ChargeSlotsNotLegal(int chargeSlots)
         {
             this.chargeSlots = chargeSlots;
         }
+
+        /// <summary>
+        /// returns a matching string for the exption
+        /// </summary>
+        /// <returns>a printable matching string</returns>
         public override string ToString()
         {
             return "ChargeSlotsNotLegal: The station cann't add/update, because the chargeSlots is not legal (" + chargeSlots + ")\n";
