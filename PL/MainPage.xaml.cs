63bb692fe62d4a7057c298b38299e6e298a196df
israel <isracohe@g.jct.ac.il>
2021-12-03 13:04:18 +0200

    israel

--- PL/MainPage.xaml.cs
+++ PL/MainPage.xaml.cs
@@ -24,9 +24,9 @@ namespace PL
         internal MainWindow mWindow;
 
         /// <summary>
-        /// 
+        /// the constructor of the main page 
         /// </summary>
-        /// <param name="mainWindow"></param>
+        /// <param name="mainWindow">the main window</param>
         public MainPage(MainWindow mainWindow)
         {
             mainWindow.CurrentPageBonus = typeof(MainPage);
@@ -35,12 +35,22 @@ namespace PL
             mWindow = mainWindow;
         }
 
+        /// <summary>
+        /// when the show drone button has been pressed it navigates to the wanted page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ShowDronesButton_Click(object sender, RoutedEventArgs e)
         {
             mWindow.CurrentPageBonus = typeof(DroneListPage);
             this.NavigationService.Navigate(new DroneListPage(bl, this));
         }
 
+        /// <summary>
+        /// makes sure that the gif keep running over and over again
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Again_MediaEnded(object sender, RoutedEventArgs e)
         {
             Gif.Position = new TimeSpan(0, 0, 1);
