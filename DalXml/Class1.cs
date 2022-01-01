5ad9bcf514029d52eaa4e3b527c772f3d4146caa
israel <isracohe@g.jct.ac.il>
2022-01-01 22:56:39 +0200

    israel

--- DalXml/Class1.cs
+++ DalXml/Class1.cs
@@ -3,12 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
 using DO;
 using DalApi;
 
 namespace Dal
 {
-    class Class1
+    internal sealed partial class DalXml
     {
+        public DalXml() { }
+        Dictionary<Type, string> fileNames = new Dictionary<Type, string>() {
+                {typeof(Customer) , "Customers.xml"},
+                {typeof(Drone) , "Drones.xml"},
+                {typeof(DroneCharge) , "DronesCharges.xml"},
+                {typeof(Parcel) , "Parcels.xml"},
+                {typeof(Station) , "Stations.xml"},
+            };
+
+        List<T> Read<T>() {
+            XmlReader Reader;
+            List<T> Data;
+            XmlSerializer Ser = new XmlSerializer(typeof(List<T>));
+            
+
+            try {
+                Reader = new XmlTextReader(Path.Combine("Data", fileNames[typeof(T)]));
+
+                if (Ser.CanDeserialize(Reader)) {
+                    Data = (List<T>)Ser.Deserialize(Reader);
+                }
+                else {
+                    Data = new List<T>();
+                }
+                Reader.Close();
+
+                return Data;
+            }
+            catch (DirectoryNotFoundException) {
+                Directory.CreateDirectory("Data");
+                return new List<T>();
+            }
+            catch (FileNotFoundException) {
+                return new List<T>();
+            }
+            catch (XmlException) {
+                return new List<T>();
+            }
+        }
+        void Write<T>(List<T> data) {
+            XmlSerializer ser = new XmlSerializer(typeof(List<T>));
+
+            TextWriter Writer;
+            try {
+                Writer = new StreamWriter(Path.Combine("Data", fileNames[typeof(T)]));
+            }
+            catch (DirectoryNotFoundException) {
+                Directory.CreateDirectory("Data");
+                Writer = new StreamWriter(Path.Combine("Data", fileNames[typeof(T)]));
+            }
+            try {
+                ser.Serialize(Writer, data);
+            }
+            catch (Exception err) { throw err; }
+            finally {
+                Writer.Close();
+            }
+        }
+
+        static int Update<T>(List<T> listy, T updater) {
+            var Id = typeof(T).GetProperty("Id");
+
+            int index = listy.FindIndex(x => (int)Id.GetValue(x, null) == (int)Id.GetValue(updater, null));
+
+            if (index != -1)
+                listy[index] = updater;
+
+            return index;
+        }
     }
 }
