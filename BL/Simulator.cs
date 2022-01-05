80b9028c4879c7130ead493e6f1c26e9524383d8
Eliyahu Gamliel <egamliel@g.jct.ac.il>
2022-01-05 11:56:58 +0200

    PR04

--- BL/Simulator.cs
+++ BL/Simulator.cs
@@ -10,29 +10,50 @@ namespace BL
 {
     class Simulator
     {
-        double DELAY = 500;
+        int DELAY = 500;
         double SPEED = 100;
-        Simulator(BL bl, int Id, Action<BO.ParcelTransfer> updateDrone, Func<bool> stop) {
+        Simulator(BL bl, int Id, Action<int> updateDrone, Func<bool> stop) {
             while (!stop()) {
                 BO.Drone drone = bl.GetDroneById(Id);
                 if (drone.Status == DroneStatuses.Delivery) {
-
+                    BO.Parcel parcel = bl.GetParcelById(drone.PTransfer.Id);
+                    if (parcel.PickedUp == null) {
+                        bl.PickUpDroneParcel(Id);
+                        updateDrone(Id);
+                        Thread.Sleep(DELAY);
+                    }
+                    else {
+                        bl.DeliverParcelCustomer(Id);
+                        updateDrone(Id);
+                        Thread.Sleep(DELAY);
+                    }
                 }
                 else if (drone.Status == DroneStatuses.Available) {
                     try {
                         bl.AssignDroneParcel(Id);
+                        updateDrone(Id);
+                        Thread.Sleep(DELAY);
                     }
                     catch {
                         try {
                             bl.SendDrone(Id);
+                            updateDrone(Id);
+                            Thread.Sleep(DELAY);
                         }
                         catch {
-
+                            Thread.Sleep(DELAY);
                         }
                     }
                 }
                 else {
-
+                    if (drone.Battery == 100) {
+                        bl.ReleasDrone(Id);
+                        updateDrone(Id);
+                        Thread.Sleep(DELAY);
+                    }
+                    else {
+                        Thread.Sleep(DELAY);
+                    }
                 }
             }
         }
